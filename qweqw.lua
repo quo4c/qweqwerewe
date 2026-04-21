313136a11b8efef417fca2e66c081224c73a1b72
quo4c <137902720+quo4c@users.noreply.github.com>
2026-04-21 21:49:53 +0400

    Update qweqw.lua

--- qweqw.lua
+++ qweqw.lua
@@ -1,19 +1,2 @@
-_G.HoHoCustomConfig = {
-    SelectTeam = "Marine",
-    BypassTeleport = false,
-    PortalTeleport = true,
-    LeavePortalUlti = true,
-    FPSBoost = false,
-    WhiteScreen = false,
-    AutoAlly = true,
-    NoClip = true,
-    NoStun = true,
-    AutoCollectFruit = true,
-    AutoStoreFruit = true,
-    AutoRandomFruit = true,
-    CheckFruitStoreable = true,
-    Webhook = "",
-    SendWebhookNewItem = true,
-    FarmWithHop = true,
-}
+
 loadstring(game:HttpGet("https://raw.githubusercontent.com/acsu123/HOHO_H/main/Loading_UI"))()
