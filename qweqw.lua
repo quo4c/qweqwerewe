758ad005b52f4b6bfbcc90ba0aa76861d034c3ba
quo4c <137902720+quo4c@users.noreply.github.com>
2026-06-24 00:47:47 +0400

    Update qweqw.lua

--- qweqw.lua
+++ qweqw.lua
@@ -1,2 +1,2 @@
-_G.Key = "otNlKfSQpcMkrcLRObcsSXSJgZGJmVIk"
+_G.MY_KEY_IS = "otNlKfSQpcMkrcLRObcsSXSJgZGJmVIk"
 loadstring(game:HttpGet("https://raw.githubusercontent.com/acsu123/HOHO_H/main/Loading_UI"))()
