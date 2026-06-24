16d1194e204e2f63502b4dbe569c7db55c06a2a0
quo4c <137902720+quo4c@users.noreply.github.com>
2026-06-24 19:31:21 +0400

    Add webhook URLs and configuration for HoHo Hub

--- qweqw.lua
+++ qweqw.lua
@@ -1,1 +1,12 @@
+_G["HoHo Hub Grow A Garden"] = {
+    ["Rare pet webhook URL"] = "https://discord.com/api/webhooks/1519364173455294574/v_ek0BPuAVdob_jge-7GL6w3kQCKrAauneZ5T63RuLeyOUibJdoJEiHmlhqi4UECeyrn",
+
+    ["Sent pet webhook URL"] = "https://discord.com/api/webhooks/1519364173455294574/v_ek0BPuAVdob_jge-7GL6w3kQCKrAauneZ5T63RuLeyOUibJdoJEiHmlhqi4UECeyrn",
+
+    ["Sent seed webhook URL"] = "https://discord.com/api/webhooks/1519364173455294574/v_ek0BPuAVdob_jge-7GL6w3kQCKrAauneZ5T63RuLeyOUibJdoJEiHmlhqi4UECeyrn",
+
+    ["Webhook username"] = "Garden Bot",
+
+    ["Garden Feature Test Pan"] = true
+}
 loadstring(game:HttpGet("https://raw.githubusercontent.com/acsu123/HOHO_H/main/Loading_UI"))()
